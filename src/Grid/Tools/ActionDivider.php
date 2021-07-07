98d15258966aea563a3ed5bf80d4cb4fe8e9fb5e
caserwiki <281458722@qq.com>
2021-07-07 14:59:38 +0800

    batch action divider

---
+++ src/Grid/Tools/ActionDivider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zx\Admin\Grid\Tools;
+
+use Zx\Admin\Grid\BatchAction;
+
+class ActionDivider extends BatchAction
+{
+    public function render()
+    {
+        return '';
+    }
+}
